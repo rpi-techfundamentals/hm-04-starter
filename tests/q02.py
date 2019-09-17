7cea6d09ffc55c4f4b8666609d0c9620b19919d3
Jason Kuruzovich <jkuruzovich@gmail.com>
2019-09-17 14:59:33 -0400

    Update tests

---
+++ tests/q02.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'Question',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> god.check_images_same('./fig/hm4-2.png', 'hm4-2.png')
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
