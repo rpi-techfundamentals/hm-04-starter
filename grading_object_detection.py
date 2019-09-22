77ac8d21ca56b85ff8cae3eeb9f99a136ea55699
Jason Kuruzovich <jkuruzovich@gmail.com>
2019-09-22 15:12:05 -0400

    updates

--- grading_object_detection.py
+++ grading_object_detection.py
@@ -15,13 +15,12 @@ def check_images_same(image1, image2):
     else:
         return False
 
-def difference(image1, image2, threshold=1000):
+def difference(image1, image2, threshold=300000):
     image1_cv = cv2.imread(image1)
     image2_cv = cv2.imread(image2)
     if image1_cv.shape == image2_cv.shape:
         difference = cv2.subtract( image1_cv, image2_cv)
         difference = difference.sum(axis=1).sum(axis=0).sum(axis=0)
-        print ("The difference between the images is: ", difference)
         if difference < threshold:
             return True
         else:
